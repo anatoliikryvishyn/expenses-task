3e9630112f4c96bae62c30a818d91ed47eabb5d9
Anatolii Kryvishyn <9845250+anatoliikryvishyn@users.noreply.github.com>
2022-08-22 19:22:19 +0300

    add account model with relations and validations

---
+++ backend/app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ApplicationRecord
+	has_many :expenses, dependent: :destroy
+
+	validates :name, :number, :balance, presence: true
+	validates :balance, numericality: { greater_than: 0, only_integer: true }
+end
