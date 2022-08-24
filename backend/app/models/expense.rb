ffe7fefd37ccffde2edd54823bafce1c7ba52dcd
Anatolii Kryvishyn <9845250+anatoliikryvishyn@users.noreply.github.com>
2022-08-24 10:10:02 +0300

    fix idents

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-	belongs_to :account
+  belongs_to :account
 
   validates :amount, :date, :description, presence: true
   validates :amount, numericality: { greater_than: 0, only_integer: true }
--- backend/db/migrate/20220822155911_create_accounts.rb
+++ backend/db/migrate/20220822155911_create_accounts.rb
@@ -1,11 +1,11 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
-			t.string :name
-			t.string :number
-			t.integer :balance
+      t.string :name
+      t.string :number
+      t.integer :balance
 
-			t.timestamps
+      t.timestamps
     end
   end
 end
