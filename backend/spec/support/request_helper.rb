b12ba90321d98a9013fb5ded312cddeefac562f7
Anatolii Kryvishyn <9845250+anatoliikryvishyn@users.noreply.github.com>
2022-08-24 16:31:46 +0300

    fix idents

--- backend/spec/support/request_helper.rb
+++ backend/spec/support/request_helper.rb
@@ -2,18 +2,6 @@ module RequestHelper
   def json_body
     JSON.parse(response.body)
   end
-
-  def access_denied_error
-    [{ "errors" => ["Access Denied"] }, 405]
-  end
-
-  def unauthorised_error
-    [{ "errors" => ["Access Denied"] }, 401]
-  end
-
-  def custom_error(err)
-    [{ "errors" => err }, 422]
-  end
 end
 
 RSpec.configure do |config|
